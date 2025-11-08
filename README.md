8e5ac611f3d29903caf77c879ff30b426d131ebd
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-08 19:15:25 +0100

    added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# game-of-life
+
+Group members: Peter Bollhorn
+
+Link to deployment on GitHub pages: https://pbollhorn.github.io/game-of-life
--- controller.js
+++ controller.js
@@ -1,8 +1,16 @@
 import * as view from "./view.js";
+// import Grid from "https://github.com/pbollhorn/datastruktur-portfolio/blob/main/grid/grid.js";
+// import Grid from "https://raw.githubusercontent.com/pbollhorn/datastruktur-portfolio/main/grid/grid.js";
+// import Grid from "https://cors-anywhere.herokuapp.com/https://github.com/pbollhorn/datastruktur-portfolio/blob/main/grid/grid.js";
+// import Grid from "https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/pbollhorn/datastruktur-portfolio/main/grid/grid.js";
+import Grid from "https://pbollhorn.github.io/omvendt-gaet-et-tal/grid.js";
 
 function startController() {
   console.log("hello from startController");
   view.registerEventHandlers();
+
+  const myGrid = new Grid(5, 3);
+  myGrid.printGrid();
 }
 
 startController();
