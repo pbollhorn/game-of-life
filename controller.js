428b58ad274b957cef8dd28fcf997fda31d6e6cf
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-08 19:57:44 +0100

    progression

--- controller.js
+++ controller.js
@@ -2,12 +2,14 @@
 import Grid from "https://pbollhorn.github.io/datastruktur-portfolio/grid/grid.js";
 
 function startController() {
-  console.log("Goodbye from startController");
+  console.log("Hello from startController");
   // view.registerEventHandlers();
 
   const myGrid = new Grid(5, 3);
   console.log(myGrid);
   myGrid.printGrid();
+
+  console.log("Goodbye startController");
 }
 
 startController();
