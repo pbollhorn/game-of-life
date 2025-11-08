570db332f1ca39f19194cfec97396d3fefcaeea0
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-08 19:21:38 +0100

    test4

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ import * as view from "./view.js";
 // import Grid from "https://raw.githubusercontent.com/pbollhorn/datastruktur-portfolio/main/grid/grid.js";
 // import Grid from "https://cors-anywhere.herokuapp.com/https://github.com/pbollhorn/datastruktur-portfolio/blob/main/grid/grid.js";
 // import Grid from "https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/pbollhorn/datastruktur-portfolio/main/grid/grid.js";
-import Grid from "https://pbollhorn.github.io/omvendt-gaet-et-tal/grid.js";
+// import Grid from "https://pbollhorn.github.io/omvendt-gaet-et-tal/grid.js";
 
 function startController() {
   console.log("hello from startController");
@@ -12,7 +12,7 @@ function startController() {
   // const myGrid = new Grid(5, 3);
   // console.log(myGrid);
   // myGrid.printGrid();
-  console.log("Goodbye startController");
+  console.log("Goodbye from startController");
 }
 
 startController();
