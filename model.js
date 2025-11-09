28b8df241f3fb77f5d7ffe44b8a4be69c6f20691
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-09 11:24:57 +0100

    progression

--- model.js
+++ model.js
@@ -1,18 +1,22 @@
 import Grid from "https://pbollhorn.github.io/datastruktur-portfolio/grid/grid.js";
 
+let grid;
+
 export function startModel() {
   console.log("Hello from startModel");
 
-  const myGrid = new Grid(5, 3);
-  console.log(myGrid);
+  // const myGrid = new Grid(5, 3);
+  // console.log(myGrid);
 
-  myGrid.printGrid();
+  // myGrid.printGrid();
 
   console.log("Goodbye from startModel");
 }
 
 export function resetBoard(rows, cols) {
   console.log("From model: resetting board with rows", rows, "cols ", cols);
+  grid = new Grid(rows, cols);
+  grid.printGrid();
 }
 
 export function setCell(row, col) {
