b6341fccc76ed9fdfb4783c8668bef056f5ee110
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-09 18:28:37 +0100

    updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Group members: Peter Bollhorn
 
 Link to deployment on GitHub pages: https://pbollhorn.github.io/game-of-life
+
+Features:
+- Chose number of rows and columns for the board
+- Manually set cells before simulation start
+- Simulation can run, but it seems to have bugs
--- model.js
+++ model.js
@@ -44,6 +44,6 @@ function countLiveNeighbours(row, col) {
 export function doesCellLive(row, col) {
   const liveNeighbourCount = countLiveNeighbours(row, col);
   if (liveNeighbourCount < 2) return false;
-  if (liveNeighbourCount < 3) return false;
+  if (liveNeighbourCount > 3) return false;
   return true;
 }
